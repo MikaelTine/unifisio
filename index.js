a1f677de0b94d66315eacaec1600f336a9f632b1
Mikael Tine <61232416+MikaelTine@users.noreply.github.com>
2023-06-23 10:03:32 -0300

    23/06 10:03

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 import Head from 'next/head'; 
 
 export default function Home() {
+
+    const [data, setData] = useState({
+        name: '',
+        cpf: ''
+    });
+
+    const valorInput = e => setData({...data, [e.target.name]: e.target.value})
+
+    const sendMsg = (e) => {
+
+        console.log(`Nome: ${data.name}`);
+
+    }
+
     return (
         <>
             <Head>
@@ -9,12 +23,18 @@ export default function Home() {
                 <meta nome="viewport" content="widh=device-width, initial-scale=1" />
             </Head>
             <main>
-                <form>
+
+                <h2>FORMULARIO</h2>
+
+                <form onSubmit={sendMsg}>
+
                     <label>Nome: </label>
-                    <input type="text" name="name" placeholder='Digite o nome' /><br /> <br />
+                    <input type="text" name="name" placeholder='Digite o nome' onChange={valorInput} required/><br /> <br />
 
                     <label>CPF: </label>
-                    <input type="number" name="cpf" placeholder='Digite o CPF' /><br /> <br />
+                    <input type="number" name="cpf" placeholder='Digite o CPF' onChange={valorInput} required/><br /> <br />
+
+                    <button type="submit">Enviar</button><br /><br />
                 </form> 
             </main>             
         </>
