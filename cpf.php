7082fb6cd5b83edae9955ab263038668019b9f58
Mikael Tine <61232416+MikaelTine@users.noreply.github.com>
2023-06-26 18:30:53 -0300

    18:30   26/06/2023

---
+++ cpf.php
@@ -0,0 +1,25 @@
+<?php
+
+$cpf = $_POST['cpf'];
+
+$url = "https://api.medicinadireta.com.br/odata/Paciente/Filters(id=0, nome='', cpf='', limit=100, offset=1)?$select=id,nome,cpf&$count=true".$cpf."&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJDRU5UUk8gREUgRklTSU9URVJBUElBIEUgUkVBQklMSVRBw4fDg08gQ0FWRU5BR0hJIFMvUyIsImp0aSI6ImUxY2M4OTY1LTFhOTAtNDZkYS05NTBmLTExNmVkMjI0ZDQwZiIsIk9yZ2FuaXphY2FvSWQiOiIyNjEyIiwiVXNlcklkIjoiMTkxNzUiLCJleHAiOjE2ODc4MzI4MDcsImlzcyI6Ik1lZGljaW5hRGlyZXRhLkFQSSIsImF1ZCI6Ik1lZGljaW5hRGlyZXRhLkFQSSJ9.ajkIc6wP9nyprhXDu1IQECAeSTSy-q-pRWjt8Mu1h6w";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_TIMEOUT,450);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT ,10);
+$result = curl_exec($ch);
+curl_close($ch);
+
+if(@$result -> return == "OK" ){
+
+        echo @
+
+
+
+}
+
+?>
